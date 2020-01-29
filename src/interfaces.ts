153d4fc1aa81bf14156842172a26d0808cef5c3a
Balte de Wit <balte.dewit@gmail.com>
2020-01-29 19:08:31 +0100

    wip

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,6 +8,10 @@ export interface UserInputEvent {
 	values: { [input: string]: number | boolean }, // something like joysticks.left.x: 0.5, joysticks.left.y: 1
 }
 
+export interface DeviceHandler {
+	handleFeedback: (instance: FeedbackInstance) => boolean
+}
+
 export enum DeviceType {
 	Mouse,
 	Controller
@@ -19,62 +23,95 @@ export enum ValueType {
 	Delta // positive or minus
 }
 
-export interface UserInputEventManifest {
+
+export interface UserInputDeviceManifest {
 	deviceType: DeviceType
-	inputs: { [identifier: string]: UserInputManifest }
+	inputInstances: Array<UserInputInstanceManifest>
 }
 
-export interface UserInputManifest {
-	[identifier: string]: UserInputManifest | ValueType
-}
-
-export const mouseExample: UserInputEventManifest = {
-	deviceType: DeviceType.Mouse,
-	inputs: {
-		scrollWheels: {
-			x: ValueType.Delta,
-			y: ValueType.Delta
-		},
-		buttons: {
-			left: ValueType.Boolean,
-			right: ValueType.Boolean,
-			Middle: ValueType.Boolean
-		},
-		movement: {
-			x: ValueType.Delta,
-			y: ValueType.Delta
-		}
+export interface UserInputInstanceManifest {
+	identifier: string,
+	arguments: {
+		[path: string]: ValueType
 	}
 }
 
-export const xboxExample: UserInputEventManifest = {
-	deviceType: DeviceType.Controller,
-	inputs: {
-		joysticks: {
-			left: {
-				x: ValueType.Range,
-				y: ValueType.Range
-			},
-			right: {
-				x: ValueType.Range,
-				y: ValueType.Range
+export enum FeedbackType {
+	State,
+	Colour,
+	Text
+}
+
+export interface FeedbackInstance {
+	type: FeedbackType,
+	values: any
+}
+
+export const mouseExample: UserInputDeviceManifest = {
+	deviceType: DeviceType.Mouse,
+	inputInstances: [
+		{
+			identifier: 'verticalScroll',
+			arguments: {
+				delta: ValueType.Delta
 			}
 		},
-		triggers: {
-			left: ValueType.Range,
-			right: ValueType.Range
+		{
+			identifier: 'horizontalScroll',
+			arguments: {
+				delta: ValueType.Delta
+			}
 		},
-		buttons: {
-			dpad: {
-				up: ValueType.Boolean,
-				down: ValueType.Boolean,
-				left: ValueType.Boolean,
-				right: ValueType.Boolean,
-			},
-			a: ValueType.Boolean,
-			b: ValueType.Boolean,
-			x: ValueType.Boolean,
-			y: ValueType.Boolean
+		{
+			identifier: 'mouseMove',
+			arguments: {
+				x: ValueType.Delta,
+				y: ValueType.Delta
+			}
+		},
+		{
+			identifier: 'leftClick',
+			arguments: {}
+		},
+		{
+			identifier: 'middleClick',
+			arguments: {}
+		},
+		{
+			identifier: 'rightClick',
+			arguments: {}
 		}
-	}
+	]
 }
+
+// export const xboxExample: UserInputEventManifest = {
+// 	deviceType: DeviceType.Controller,
+// 	inputs: {
+// 		joysticks: {
+// 			left: {
+// 				x: ValueType.Range,
+// 				y: ValueType.Range
+// 			},
+// 			right: {
+// 				x: ValueType.Range,
+// 				y: ValueType.Range
+// 			}
+// 		},
+// 		triggers: {
+// 			left: ValueType.Range,
+// 			right: ValueType.Range
+// 		},
+// 		buttons: {
+// 			dpad: {
+// 				up: ValueType.Boolean,
+// 				down: ValueType.Boolean,
+// 				left: ValueType.Boolean,
+// 				right: ValueType.Boolean,
+// 			},
+// 			a: ValueType.Boolean,
+// 			b: ValueType.Boolean,
+// 			x: ValueType.Boolean,
+// 			y: ValueType.Boolean
+// 		}
+// 	}
+// }
